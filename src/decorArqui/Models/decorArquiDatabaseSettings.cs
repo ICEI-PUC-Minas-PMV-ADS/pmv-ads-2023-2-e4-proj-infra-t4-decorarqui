c7b0c4c9151768aa4caffdc9d6f645e10cb53fe4
Pedro Ertal <pedro.ertal@yahoo.com.br>
2023-10-20 12:52:39 -0300

    Pasta SRC Correta

--- src/decorArqui/Models/decorArquiDatabaseSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace decorArqui.Models
-{
-    public class decorArquiDatabaseSettings
-    {
-        public string? ConnectionString { get; set; }
-        public string? DatabaseName { get; set;}
-        public string? CollectionName { get; set;}
-    }
-}
