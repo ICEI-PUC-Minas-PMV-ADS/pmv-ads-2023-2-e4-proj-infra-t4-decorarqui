0e8b328ae812220aeb2dfe51224c666c780b7a24
Pedro Ertal <pedro.ertal@yahoo.com.br>
2023-10-23 14:24:58 -0300

    Página Cadastro e Index estilizadas

--- src/decorArqui/Program.cs
+++ src/decorArqui/Program.cs
@@ -64,6 +64,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Login}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
